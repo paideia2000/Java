a76fb5dec002c6374fa203f8520453f4cca315b4
Rene <poencima22@gmail.com>
2025-05-21 23:59:51 +0200

    loops and conditionals in java

--- Learning_Java/src/Main.java
+++ Learning_Java/src/Main.java
@@ -5,10 +5,31 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int invalidtry = 0;
+        while (invalidtry < 3) {
+            System.out.print("Enter a numbers please: ");
 
-        
+            if (input.hasNextInt()) {
+                int number = input.nextInt();
+                System.out.printf("The number is %d\n", number);
+                break;
+            } else if (input.hasNextDouble()) {
+                double number = input.nextDouble();
+                System.out.printf("The number is %.2f\n", number);
+                break;
+            } else {
+                String value = input.nextLine();
+                System.out.printf("The value '%s' isin't a number\n", value);
+                invalidtry++;
+
+            }
+        }
+        System.out.println("Close the bucle");
+        input.close();
     }
 }
+
 
 
