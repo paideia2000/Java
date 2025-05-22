5f89af274f1c037309bc4138c708f6ea524a5775
Rene <poencima22@gmail.com>
2025-05-22 11:52:23 +0200

    details in css

--- Learning_Java/src/Main.java
+++ Learning_Java/src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.Text;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -5,29 +7,8 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        int invalidtry = 0;
-        while (invalidtry < 3) {
-            System.out.print("Enter a numbers please: ");
 
-            if (input.hasNextInt()) {
-                int number = input.nextInt();
-                System.out.printf("The number is %d\n", number);
-                break;
-            } else if (input.hasNextDouble()) {
-                double number = input.nextDouble();
-                System.out.printf("The number is %.2f\n", number);
-                break;
-            } else {
-                String value = input.nextLine();
-                System.out.printf("The value '%s' isin't a number\n", value);
-                invalidtry++;
-
-            }
-        }
-        System.out.println("Close the bucle");
-        input.close();
     }
 }
 
