af001e5f6345b310cedf509c248a7c33adf543d6
Rene <poencima22@gmail.com>
2025-05-22 17:09:21 +0200

    displays

--- Learning_Java/src/Main.java
+++ Learning_Java/src/Main.java
@@ -1,6 +1,3 @@
-import org.w3c.dom.Text;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,7 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
 
+        int amountry = 0;
+        while (amountry < 3) {
+            System.out.print("\nEnter your age: ");
+
+            if (input.hasNextInt()){
+                int age = input.nextInt();
+                System.out.printf("\nYour age is '%d' \n", age);
+                break;
+            } else if (input.hasNextDouble()) {
+                double invalid_value = input.nextDouble();
+                System.out.printf("\nThe age must be a integer number, not decimal '%.1f'\n", invalid_value);
+                amountry++;
+            }else {
+                input.nextLine();
+                System.out.println("\nERROR: Try Again please.");
+                amountry++;
+            }
+
+        }
+        System.out.print("\nLoop Closed.\n");
+        input.close();
     }
 }
 
