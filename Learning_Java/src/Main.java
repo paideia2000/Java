8a8b2399ea3e726a8543c21e5d40ea642ad0c9fa
Rene <poencima22@gmail.com>
2025-05-26 20:23:57 +0200

    more exercise in html

--- Learning_Java/src/Main.java
+++ Learning_Java/src/Main.java
@@ -7,43 +7,31 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        final int weightClown = 112;
-        final int weightDolls = 75;
+        final double discountOldBread = 0.6;
+        final double priceBread = 3.49;
 
-        int amountClown;
-        int amountDolls;
+        int amountOldBreadBuy;
 
         while (true) {
-            System.out.print("\nEnter the numbers of clown sell: ");
-            if (input.hasNextInt()) {
-                amountClown = input.nextInt();
+            System.out.print("\nEnter the number of loaves sold that are not from the day: ");
+            if (input.hasNextInt()){
+                amountOldBreadBuy = input.nextInt();
                 input.nextLine();
                 break;
-            } else {
-                System.out.println("ERROR: You must insert an integer.");
+            }else {
+                System.out.println("ERROR: Check the value inserted.");
                 input.nextLine();
             }
         }
 
-        while (true) {
-            System.out.print("\nEnter the number of the dolls sell: ");
-            if (input.hasNextInt()) {
-                amountDolls = input.nextInt();
-                break;
-            } else {
-                System.out.println("ERROR: You must insert an integer.");
-                input.nextLine();
-            }
+        try{
+
+            double totalValue = amountOldBreadBuy * priceBread *  (1 - discountOldBread);
+            System.out.printf("\nHas comprado %d bread a un precio de %.2f con un descuento de %.2f porciento por lo que tienes que pagar %.2f euros", amountOldBreadBuy, priceBread, discountOldBread, totalValue);
+
+        } catch (ArithmeticException e){
+            System.err.println("An Error ocurred: " + e.getMessage());
         }
-
-        try {
-            double totalweight = (weightClown * amountClown) + (weightDolls * amountDolls);
-
-            System.out.printf("The total weight oh the package is: %.2f lb", totalweight);
-        } catch (Exception e) {
-            System.err.println("An error ocurred: " + e.getMessage());;
-        }
-
     }
 }
 
