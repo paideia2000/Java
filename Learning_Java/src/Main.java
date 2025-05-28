b1e07071cf41129f662958f10982e5aa5b905b10
Rene <poencima22@gmail.com>
2025-05-28 20:27:40 +0200

    more exercise

--- Learning_Java/src/Main.java
+++ Learning_Java/src/Main.java
@@ -1,37 +1,24 @@
+
 import java.util.Locale;
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Locale.setDefault(Locale.US);
 
         Scanner input = new Scanner(System.in);
 
-        final double discountOldBread = 0.6;
-        final double priceBread = 3.49;
+        int contador = 0;
+        while (contador < 0){
 
-        int amountOldBreadBuy;
+            System.out.print("Enter your name: ");
+            String name = input.nextLine();
+            if (name.matches("A-Za-z"+)){
 
-        while (true) {
-            System.out.print("\nEnter the number of loaves sold that are not from the day: ");
-            if (input.hasNextInt()){
-                amountOldBreadBuy = input.nextInt();
-                input.nextLine();
-                break;
-            }else {
-                System.out.println("ERROR: Check the value inserted.");
-                input.nextLine();
             }
         }
 
-        try{
-
-            double totalValue = amountOldBreadBuy * priceBread *  (1 - discountOldBread);
-            System.out.printf("\nHas comprado %d bread a un precio de %.2f con un descuento de %.2f porciento por lo que tienes que pagar %.2f euros", amountOldBreadBuy, priceBread, discountOldBread, totalValue);
-
-        } catch (ArithmeticException e){
-            System.err.println("An Error ocurred: " + e.getMessage());
-        }
     }
 }
 
--- test.py
+++ test.py
@@ -1,1 +1,33 @@
-print((10 * 3.49) / 60)
+import re
+
+
+try:
+    
+    full_name = input("\nEnter your full name, follow this format (rene medina perez): ")
+    
+    if re.fullmatch(r'[A-Za-z ]+', full_name):
+        
+        list_split = [word for word in full_name.split()]
+        new_lis = []
+        
+        
+        for word in list_split:
+            for w in word:
+                if w[0].islower():
+                    new_lis.append(word[0].upper() + word[1:])
+                    break
+                else:
+                    continue
+        
+        
+        print(" ".join(new_lis))
+
+    else:
+        
+        raise ValueError("Check teh fiel.")
+    
+    
+    
+
+except ValueError as vl:
+    print("An Error Ocurrer: {}".format(vl))
